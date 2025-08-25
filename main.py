233e162e532da46d8315de96a6f01ee86ff7561d
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2025-08-25 16:03:13 -0300

    Alteração no horário da execução do script

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def rodar_auditoria():
     print(f"Rodando auditoria: {datetime.now()}")
     page = pageSpeed('https://pageSpeed.web.dev', '')
     page.aplica_site()
+rodar_auditoria()
 
 
 schedule.every().day.at("08:20").do(rodar_auditoria)
